8debb070bb533ee4c37b307a666b398ce68a13ef
Sagar Padekar <sagar.padekar@gmail.com>
2026-09-18 20:06:49 +0100

    Add Spring Boot Java 21 application scaffold

---
+++ src/main/java/com/mdb/petstore/PetstoreModernizedApplication.java
@@ -0,0 +1,13 @@
+package com.mdb.petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreModernizedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetstoreModernizedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mdb/petstore/PetstoreModernizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdb.petstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreModernizedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
